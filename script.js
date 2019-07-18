f8e03887d9bc05d87971f860c3210922cecef2c4
Gregor <gregorlthomson@sky.com>
2019-07-18 16:50:39 +0100

    Clean up script.js file

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// Define Buttons & Default for Undefined
 var button = document.querySelector("#button-submit");
 var buttonClear = document.querySelector("#button-clear");
 var found = true;
+
+// Recipe Elements
+var recipeImg = document.querySelector("#recipe-img");
+var recipeIngredients = document.querySelector("#recipe-ingredients");
+var recipeLink = document.querySelector("#recipe-link");
+
+// Playlist Elements
+var image = document.querySelector("#playlist-img");
+var playlist = document.querySelector("#playlist-songs");
 
 // Search Functionality
 button.addEventListener("click", function() {
@@ -29,29 +39,31 @@ function recipeCall(searchTerm) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var recipeObj = JSON.parse(xhr.responseText);
-      var recipeImg = document.querySelector("#recipe-img");
-      var recipeIngredients = document.querySelector("#recipe-ingredients");
-      var recipeLink = document.querySelector("#recipe-link");
-      var randomRecipe = chooseRandom(recipeObj.results);
+      var randomRecipe = chooseRandom(recipeObj.results); // Choose a random result
+
+      // Check if there are no results
       if (randomRecipe == undefined) {
         alert("Please put in a proper party theme!");
         var searchInput = document.querySelector("#search-query");
         searchInput.value = "";
         found = false;
       }
+      // Split ingredients list into items
       var ingredients = randomRecipe.ingredients.split(",");
 
+      // Function for creating list items
       function listCreation(str) {
         var listItem = document.createElement("li");
         listItem.textContent = str;
         recipeIngredients.appendChild(listItem);
       }
-
+      // Function to add each ingredient
       function addIngredients() {
         recipeIngredients.textContent = "";
         ingredients.forEach(c => listCreation(c));
       }
 
+      // Check for thumbnail and resort to default if unavailable
       if (randomRecipe.thumbnail) {
         recipeImg.src = randomRecipe.thumbnail;
       } else {
@@ -78,19 +90,12 @@ function deezerCall(searchTerm) {
       var parsedContent = JSON.parse(xhr.responseText);
       var randomPlaylist = chooseRandom(parsedContent.data); //  Returns random playlist out of top 10
 
-      var playlistTitle = randomPlaylist.title; //  Returns selected playlist's title
-      var playlistImage = randomPlaylist.picture_medium; //  Returns selected playlist's cover
-      var playlistLink = randomPlaylist.link;
-
       //  var tracklistLink = randomPlaylist.tracklist; Returns playlist tracklist link > for second call
 
       if (found) {
-        var image = document.querySelector("#playlist-img");
-        var playlist = document.querySelector("#playlist-songs");
-
-        playlist.textContent = "Suggested playlist: " + playlistTitle;
-        image.src = playlistImage;
-        playlist.href = playlistLink;
+        playlist.textContent = randomPlaylist.title;
+        image.src = randomPlaylist.picture_medium;
+        playlist.href = randomPlaylist.link;
       }
     }
   };
